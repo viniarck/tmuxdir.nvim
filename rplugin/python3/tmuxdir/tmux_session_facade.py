19ac72c7f3d6e37a47f29ea3cd84f9a95cceef20
Vinicius Arcanjo <viniarck@gmail.com>
2020-03-20 18:52:28 -0300

    - Removed unneeded states on TmuxSessionFacade - Simplified TmuxSessionFacade func names

--- rplugin/python3/tmuxdir/tmux_session_facade.py
+++ rplugin/python3/tmuxdir/tmux_session_facade.py
@@ -47,18 +47,9 @@ class TmuxSessionFacade(object):
 
     def __init__(self) -> None:
         self._check_tmux_bin()
-        self._sessions: Dict[str, TmuxSession] = {}
 
-    def is_attached(self) -> bool:
-        """Check if the local client is attached to tmux."""
-
-        if os.environ.get("TMUX"):
-            return True
-        return False
-
-    def _sync_sessions(self) -> None:
-        """Synchronize current tmux sessions in memory."""
-
+    def sessions(self) -> Dict[str, TmuxSession]:
+        """Get existing tmux sessions."""
         sessions: Dict[str, TmuxSession] = {}
         out = self._run_cmd(
             [
@@ -73,32 +64,15 @@ class TmuxSessionFacade(object):
             if len(words) != 3:
                 raise TmuxFacadeException("Failed to parse tmux list-sessions")
             sessions[words[0]] = TmuxSession(words[0], words[1], words[2])
+        return sessions
 
-        # update
-        if list(self._sessions.keys()) != list(sessions.keys()):
-            self._sessions = sessions
-
-    def list_sessions(self) -> List[TmuxSession]:
-        """List tmux sessions.
-
-        _sync_sessions first since sessions can be created/killed by other
-        processes.
-        """
-
-        self._sync_sessions()
-        return self._sessions.values()
-
-    def exists_session(self, session_name) -> bool:
-        """Check if a session exists
-        Always _sync_sessions first since sessions can be created/killed.
-        """
-
-        self._sync_sessions()
-        if self._sessions.get(session_name):
+    def is_attached(self) -> bool:
+        """Check if the local client is attached to tmux."""
+        if os.environ.get("TMUX"):
             return True
         return False
 
-    def create_session(
+    def create(
         self,
         session_name: str,
         vim_bin_path: str,
@@ -108,66 +82,45 @@ class TmuxSessionFacade(object):
         """Create a detached tmux session with nvim/vim started
         Raises TmuxFacadeException if an err occurs."""
 
-        _cmd = ["tmux", "new-session", "-d"]
-
-        if start_directory:
-            _cmd.extend(["-c", start_directory])
-
-        _cmd.extend(["-s", session_name])
-
-        _cmd.append(vim_bin_path)
+        _cmd = [
+            "tmux",
+            "new-session",
+            "-d",
+            "-c",
+            start_directory,
+            "-s",
+            session_name,
+            vim_bin_path,
+        ]
         if vim_args:
             _cmd.extend(["-c", vim_args])
-
         self._run_cmd(_cmd)
 
-    def switch_session(self, session_name: str) -> None:
+    def switch(self, session_name: str) -> None:
         """Switch to a tmux session
         Raises TmuxFacadeException if an err occurs."""
 
         if not self.is_attached():
             raise TmuxFacadeException(
-                "Please, make sure you have a tmux session attached before trying to switch."
+                "Please, make sure you have a tmux session attached before"
+                " trying to switch."
             )
         self._run_cmd(["tmux", "switch-client", "-t", session_name])
 
-    def switch_or_create(
-        self, session_name: str, start_directory: str, vim_args: str = ""
-    ) -> None:
-        """Either switch or create a new tmux session
-        Raises TmuxFacadeException if an err occurs."""
-
-        if not self.exists_session(session_name):
-            if not self.is_attached():
-                self.create_session(
-                    session_name=session_name,
-                    detached=False,
-                    start_directory=start_directory,
-                    vim_args=vim_args,
-                )
-                return
-            else:
-                self.create_session(
-                    session_name=session_name,
-                    detached=True,
-                    start_directory=start_directory,
-                    vim_args=vim_args,
-                )
-        self.switch_or_attach(session_name=session_name)
-
-    def kill_session(self, session_name: str) -> None:
+    def kill(self, session_name: str) -> None:
         """Kill a tmux session
         Raises TmuxFacadeException if an err occurs."""
 
         self._run_cmd(["tmux", "kill-session", "-t", session_name])
 
-    def _check_tmux_bin(self) -> None:
+    def _check_tmux_bin(self) -> bool:
         """Check if tmux binary can be found in the $PATH
         Raises TmuxFacadeBinException if an err occurs."""
 
         try:
             self._run_cmd(["tmux", "-V"])
-        except TmuxFacadeException as e:
+            return True
+        except TmuxFacadeException:
             raise TmuxFacadeBinException(
                 "tmux not found in $PATH. Make sure tmux is installed."
             )
