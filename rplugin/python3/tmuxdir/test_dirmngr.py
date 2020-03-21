52bb2f0f542218e62cccd27ff92dc012040ddad5
Vinicius Arcanjo <viniarck@gmail.com>
2020-03-21 18:50:56 -0300

    Enhanced tests and added fixtures

--- rplugin/python3/tmuxdir/test_dirmngr.py
+++ rplugin/python3/tmuxdir/test_dirmngr.py
@@ -4,58 +4,77 @@ import pytest
 
 
 @pytest.fixture
-def dirmngr() -> None:
-    pass
-    config_handler = ConfigHandler()
+def dir_mngr() -> DirMngr:
+    folder_name = "/tmp/tmuxdirtest/"
+    os.makedirs(folder_name, exist_ok=True)
+    cfg_handler = ConfigHandler(folder_name=folder_name)
+    yield DirMngr([], [".git"], cfg_handler=cfg_handler)
+    try:
+        os.remove(str(cfg_handler._full_path))
+    except FileNotFoundError:
+        pass
+    os.removedirs(folder_name)
+
+
+@pytest.fixture
+def cfg_handler() -> ConfigHandler:
+    folder_name = "/tmp/tmuxdirtest/"
+    os.makedirs(folder_name, exist_ok=True)
+    config_handler = ConfigHandler(folder_name=folder_name)
+    yield config_handler
+    try:
+        os.remove(str(config_handler._full_path))
+    except FileNotFoundError:
+        pass
+    os.removedirs(folder_name)
+
+
+@pytest.fixture
+def tmp_git_folder() -> str:
+    folder_name = "/tmp/repo/.git"
+    os.makedirs(folder_name, exist_ok=True)
+    yield "/".join(folder_name.split("/")[:-1])
+    os.removedirs(folder_name)
+
 
 class TestDirManager:
-    def test_first_save(self):
+    def test_first_save(self, cfg_handler: ConfigHandler):
         data = {"dirs": {"/tmp": "/tmp"}, "ignored_dirs": {}}
-        ConfigHandler.save(data)
-        loaded = ConfigHandler.load()
+        cfg_handler.save(data)
+        loaded = cfg_handler.load()
         assert loaded == data
 
-    def test_bookmark_dir(self):
-        dirs = ConfigHandler.load()
-        dir_mngr = DirMngr([], [".git"])
+    def test_add_dir(self, dir_mngr: DirMngr, tmp_git_folder: str):
+        assert dir_mngr.add(tmp_git_folder)[0] == tmp_git_folder
+        assert dir_mngr.add("/tmp/foo") == []
+        assert tmp_git_folder in dir_mngr.dirs
 
-        new_folder = "/tmp"
-        os.makedirs(new_folder, exist_ok=True)
-        assert dir_mngr.bookmark(new_folder)
-        assert dir_mngr.dirs.keys() == dirs[dir_mngr._DIRS_KEY].keys()
+    def test_add_dir_list(self, dir_mngr: DirMngr, tmp_git_folder: str):
+        folder = "/tmp/pit/"
+        assert dir_mngr.add(folder) == []
 
-    def test_add_dir_list(self):
-        marker = ".pit"
-        dir_mngr = DirMngr([], [marker])
+    def test_add_clear(self, dir_mngr: DirMngr, tmp_git_folder: str):
+        assert dir_mngr.add(tmp_git_folder) == [tmp_git_folder]
+        assert dir_mngr.clear_added_dir(tmp_git_folder)
+        assert dir_mngr.list_dirs() == []
+        assert not dir_mngr.clear_added_dir("/tmp/random/")
+        assert tmp_git_folder not in dir_mngr.dirs
 
-        new_folder = "/tmp"
-        os.makedirs(os.path.join(new_folder, marker), exist_ok=True)
-        assert dir_mngr.add(new_folder)
-        assert new_folder in dir_mngr.list_dirs()
-
-    def test_add_ignore(self):
-        marker = ".git"
-        dir_mngr = DirMngr([], [marker])
-        new_folder = "/tmp"
-        os.makedirs(os.path.join(new_folder, marker), exist_ok=True)
-        assert dir_mngr.add(new_folder)
-        assert dir_mngr.ignore(new_folder)
-        assert dir_mngr.ignored_dirs[new_folder] == new_folder
-        assert new_folder not in dir_mngr.list_dirs()
-
-    def test_clear_ignored_dirs(self):
-        dir_mngr = DirMngr([], [".git"])
-        new_folder = "/tmp"
-        assert dir_mngr.bookmark(new_folder)
-        assert dir_mngr.ignore(new_folder)
-        dir_mngr.clear_ignored_dirs()
-        assert dir_mngr.ignored_dirs == {}
-        assert ConfigHandler.load()[dir_mngr._IGNORED_DIRS_KEY] == {}
-
-    def test_clear_bookmarked_dirs(self):
-        dir_mngr = DirMngr([], [".git"])
-        new_folder = "/tmp"
-        assert dir_mngr.bookmark(new_folder)
-        dir_mngr.clear_bookmarked_dirs()
+    def test_clear_added_dirs(self, dir_mngr: DirMngr, tmp_git_folder: str):
+        assert dir_mngr.add(tmp_git_folder)[0] == tmp_git_folder
+        assert dir_mngr.clear_added_dir(tmp_git_folder)
         assert dir_mngr.dirs == {}
-        assert ConfigHandler.load()[dir_mngr._DIRS_KEY] == {}
+        assert dir_mngr.cfg_handler.load()[dir_mngr._DIRS_KEY] == {}
+
+    def test_add_ignore(self, dir_mngr: DirMngr, tmp_git_folder: str):
+        assert dir_mngr.add(tmp_git_folder) == [tmp_git_folder]
+        assert dir_mngr.ignore(tmp_git_folder)
+        assert dir_mngr.add(tmp_git_folder) == []
+        assert tmp_git_folder not in dir_mngr.list_dirs()
+
+    def test_clear_ignored_dirs(self, dir_mngr: DirMngr, tmp_git_folder: str):
+        assert dir_mngr.add(tmp_git_folder)[0] == tmp_git_folder
+        assert dir_mngr.ignore(tmp_git_folder)
+        assert dir_mngr.clear_ignored_dirs()
+        assert dir_mngr.ignored_dirs == {}
+        assert dir_mngr.cfg_handler.load()[dir_mngr._IGNORED_DIRS_KEY] == {}
