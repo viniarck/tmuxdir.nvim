7ce9f262f63aea53d18d16f16c441f7282cf5e97
Vinicius Arcanjo <viniarck@gmail.com>
2020-03-22 14:55:50 -0300

    Compute dir_to_session_name dynamically without relying on states

--- rplugin/python3/tmuxdir/tmuxdir_facade.py
+++ rplugin/python3/tmuxdir/tmuxdir_facade.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from typing import List
+from typing import Dict, List
 from tmuxdir.tmux_session_facade import TmuxSessionFacade, TmuxFacadeException
 from tmuxdir.dirmngr import DirMngr, ProjectDir
 import os
@@ -19,6 +19,7 @@ class TmuxDirFacadeException(TmuxFacadeException):
 class TmuxDirFacade(TmuxSessionFacade, DirMngr):
 
     """TmuxDirFacade Singleton."""
+
     _instance = None
 
     def __new__(cls, *args, **kwargs):
@@ -33,22 +34,26 @@ class TmuxDirFacade(TmuxSessionFacade, DirMngr):
         TmuxSessionFacade.__init__(self)
         DirMngr.__init__(self, base_dirs=base_dirs, root_markers=root_markers)
 
-        for dir_path in self.list_dirs():
-            name = dir_path.split(os.path.sep)[-2]
-            self._session_dirs[name] = ProjectDir(name=name, start_directory=dir_path)
-
     def dir_to_session_name(self, dir_path: str) -> str:
         """Convert a directory name to tmux session name."""
         dir_split = dir_path.split(os.path.sep)
         index = -1
 
+        def projects_dict() -> Dict[str, ProjectDir]:
+            projects: Dict[str, ProjectDir] = {}
+            for dir_path in self.list_dirs():
+                name = dir_path.split(os.path.sep)[-2]
+                projects[name] = ProjectDir(name=name, start_directory=dir_path)
+            return projects
+
         def replace_dots(input_str: str, replace_with: str = "-"):
             """Replace dots since it's not allowed on tmux session names."""
             return input_str.replace(".", replace_with)
 
+        projects = projects_dict()
         # recursively find the first non conflicting-key backwards
         for i in range(index, len(dir_split) * index, index):
             key = os.path.sep.join(dir_split[i:])
-            if key and not self._session_dirs.get(key):
+            if key and not projects.get(key):
                 return replace_dots(input_str=key)
         return replace_dots(input_str=dir_path)
