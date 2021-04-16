f1d739ff9cba666af2c855063a620b9f64d47f7d
Vinicius Arcanjo <viniarck@gmail.com>
2021-04-16 09:30:54 -0300

    Removed __created_time from tmux_session source

--- rplugin/python3/denite/source/tmux_session.py
+++ rplugin/python3/denite/source/tmux_session.py
@@ -10,7 +10,7 @@ class Source(Base):
         self.name = "tmux_session"
         self.kind = "tmux_session"
         self.vim = vim
-        self.sort_by = "__created_time"
+        self.sort_by = "word"
         self.sort_reversed = True
         try:
             self.tmuxf = TmuxSessionFacade()
@@ -34,14 +34,7 @@ class Source(Base):
     def gather_candidates(self, context):
         candidates = []
         for session in self.tmuxf.sessions().values():
-            word = "{:40s} ({})".format(session.name, session.created_time)
-            candidates.append(
-                {
-                    "word": word,
-                    "__session_name": session.name,
-                    "__created_time": session.created_time,
-                }
-            )
+            candidates.append({"word": session.name})
         return sorted(
             candidates, key=lambda x: x[self.sort_by], reverse=self.sort_reversed
         )
